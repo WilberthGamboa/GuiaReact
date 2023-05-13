bc8055d2c92e5648a960f0b462c3b175cfbcc926
WilberthGamboa <wgcampos15@gmail.com>
2023-05-13 15:23:18 -0600

    componente addCategory

--- 01-GifApp/src/components/AddCategory.jsx
+++ 01-GifApp/src/components/AddCategory.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddCategory = () => {
+  const [inputValue, setinputValue] = useState("Valorant");
+  const onInputChange = ({target}) =>{
+    setinputValue(target.value);
+  }
+  const onSubmit = (e) =>{
+    e.preventDefault();
+    console.log(inputValue);
+  }
+ 
   return (
-   <input
+    <form onSubmit={onSubmit} >
+   <input onChange={onInputChange}
     type='text'
     placeholder="Buscar gifs"
-   
-   
-   
+    value={inputValue}
    >
    
    </input>
+   </form>
   )
 }
